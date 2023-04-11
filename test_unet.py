61f4f71785f046320f854dcdfd4c2e9d976c275b
Elliot Kang <kkang2097@gmail.com>
2023-04-11 04:02:19 -0700

    Create test_unet.py

---
+++ test_unet.py
@@ -0,0 +1,16 @@
+import torch
+from models.unet import UNet
+
+def test_unet(pretrained_path, test_dataset):
+    return NotImplementedError
+
+
+
+#main run-command
+
+if __name__ == "__main__":
+
+    #TODO: Elliot, code
+
+
+    pass
